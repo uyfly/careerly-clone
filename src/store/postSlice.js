2b35f939111e60eaac345217e173db147f8851db
wooyoung.kim <uyfly@naver.com>
2023-09-10 19:15:14 +0900

    reducer 구현

--- src/store/postSlice.js
+++ src/store/postSlice.js
@@ -2,10 +2,14 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const postSlice = createSlice({
   name: "post",
-  initialState: [],
+  initialState: {
+    postList: [],
+  },
   reducers: {
     create: (state, action) => {
-      state.push(action.payload);
+      state.postList.push(action.payload);
+      console.log(`현재 상태 : ${JSON.stringify(state)}`);
+      console.log(`전달받은 데이터 : ${JSON.stringify(action.payload)}`);
     },
   },
 });
