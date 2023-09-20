2f6664abf5882c9ba7606e1fc7a17729259d18ef
wooyoung.kim <uyfly@naver.com>
2023-09-20 22:37:41 +0900

    redux 사용자 상태관리

---
+++ frontend/src/store/userSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    user: null,
+    isAuthenticated: false,
+  },
+  reducers: {
+    login: (state, action) => {
+      state.user = action.payload;
+      state.isAuthenticated = true;
+    },
+    logout: (state, action) => {
+      state.user = null;
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+export const { login, logout } = userSlice.actions;
+export default userSlice.reducer;
