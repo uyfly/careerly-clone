2b3fb1b80f366e6b14081aa12a9e83f677443cbc
wooyoung.kim <uyfly@naver.com>
2023-09-20 22:38:07 +0900

    redux persist 적용

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,8 +1,31 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import postReducer from "./postSlice";
+import userReducer from "./userSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+import storage from "redux-persist/lib/storage";
+import sessionStorage from "redux-persist/es/storage/session";
 
-export default configureStore({
-  reducer: {
-    post: postReducer,
-  },
+const rootPersistConfig = {
+  key: "root",
+  storage,
+};
+
+const userPersistConfig = {
+  key: "user",
+  storage: sessionStorage,
+};
+
+const rootReducer = combineReducers({
+  user: persistReducer(userPersistConfig, userReducer),
+  post: postReducer,
 });
+
+const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: [thunk],
+});
+
+export const persistor = persistStore(store);
