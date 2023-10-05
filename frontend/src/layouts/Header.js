866e966aaa927e3d9039cf98fecc015cc5426dfc
wooyoung.kim <uyfly@naver.com>
2023-10-05 21:27:50 +0900

    메뉴 링크 추가

--- frontend/src/layouts/Header.js
+++ frontend/src/layouts/Header.js
@@ -32,7 +32,7 @@ const Header = () => {
       <div className="bg-white flex justify-between w-full h-full max-w-screen-xl mx-auto px-2 md:px-4">
         <div className="flex items-center">
           <a
-            href=""
+            href="/home"
             className="p-2 m-0 mr-2 rounded focus-visible:bg-color-slate-50 focus:outline-none focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-color-slate-500 focus-visible:ring-offset-2"
           >
             <div className="hidden md:flex">
@@ -41,25 +41,25 @@ const Header = () => {
             <img className="md:hidden h-8" src={logo} alt="커리어리 홈" />
           </a>
           <a
-            href=""
+            href="/trends"
             className="hidden md:flex h-full p-2 items-center gap-1 font-medium text-center text-sm leading-none hover:text-slate-900 text-slate-500"
           >
             <p>커리어리 트렌드</p>
           </a>
           <a
-            href=""
+            href="/qnas"
             className="hidden md:flex h-full p-2 items-center gap-1 font-medium text-center text-sm leading-none hover:text-slate-900 text-slate-500"
           >
             <p>개발자 Q&A</p>
           </a>
           <a
-            href=""
+            href="/job"
             className="hidden md:flex h-full p-2 items-center gap-1 font-medium text-center text-sm leading-none hover:text-slate-900 text-slate-500"
           >
             <p>채용 공고 매칭</p>
           </a>
           <a
-            href=""
+            href="/lounge"
             className="hidden md:flex h-full p-2 items-center gap-1 font-medium text-center text-sm leading-none hover:text-slate-900 text-slate-500"
           >
             <p>사이드 프로젝트</p>
