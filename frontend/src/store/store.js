968cf973f2cc49a4e14006432d71ed6ffb72ee9b
wooyoung.kim <uyfly@naver.com>
2023-10-06 20:43:35 +0900

    redux persist 해제

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,31 +1,31 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import postReducer from "./postSlice";
 import userReducer from "./userSlice";
-import { persistReducer, persistStore } from "redux-persist";
-import thunk from "redux-thunk";
-import storage from "redux-persist/lib/storage";
+// import { persistReducer, persistStore } from "redux-persist";
+// import thunk from "redux-thunk";
+// import storage from "redux-persist/lib/storage";
 // import storageSession from "redux-persist/lib/storage/storageSession";
 
-const rootPersistConfig = {
-  key: "root",
-  storage,
-};
+// const rootPersistConfig = {
+//   key: "root",
+//   storage,
+// };
 
-const userPersistConfig = {
-  key: "user",
-  storage,
-};
+// const userPersistConfig = {
+//   key: "user",
+//   storage,
+// };
 
 const rootReducer = combineReducers({
-  user: persistReducer(userPersistConfig, userReducer),
+  user: userReducer,
   post: postReducer,
 });
 
-const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
+// const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: [thunk],
+  reducer: rootReducer,
+  // middleware: [thunk],
 });
 
-export const persistor = persistStore(store);
+// export const persistor = persistStore(store);
