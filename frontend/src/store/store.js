d00e2010e44e3313058f3d8fc010c7e918abfa06
wooyoung.kim <uyfly@naver.com>
2023-10-07 19:38:19 +0900

    store redux persist 제거

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,31 +1,12 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import postReducer from "./postSlice";
 import userReducer from "./userSlice";
-// import { persistReducer, persistStore } from "redux-persist";
-// import thunk from "redux-thunk";
-// import storage from "redux-persist/lib/storage";
-// import storageSession from "redux-persist/lib/storage/storageSession";
-
-// const rootPersistConfig = {
-//   key: "root",
-//   storage,
-// };
-
-// const userPersistConfig = {
-//   key: "user",
-//   storage,
-// };
 
 const rootReducer = combineReducers({
   user: userReducer,
   post: postReducer,
 });
 
-// const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
-
 export const store = configureStore({
   reducer: rootReducer,
-  // middleware: [thunk],
 });
-
-// export const persistor = persistStore(store);
