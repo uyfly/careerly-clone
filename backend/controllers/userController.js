8df3aa95c53e41e0f3964406b1f18220f56df03a
wooyoung.kim <uyfly@naver.com>
2023-10-08 23:31:55 +0900

    access token 쿠키 및 카카오 API 추가

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,11 @@
 const KakaoUtil = require("../KakaoUtil");
 const kakaoUtil = new KakaoUtil();
 const { User } = require("../models/User");
+const redisClient = require("../redis");
 
 const userController = {
   /**
-   * @description 인가 코드 받기
+   * @description 카카오 인가 코드 받기
    */
   getKakaoAuthCode: (req, res) => {
     console.log("===== /kakao/oauth/authorize start =====");
@@ -19,15 +20,21 @@ const userController = {
   /**
    * @description 카카오 로그인
    */
-  getKakaoLogin: async (req, res) => {
+  kakaoLogin: async (req, res) => {
     console.log("===== /login start =====");
 
     try {
       const { code } = req.body;
-      const { access_token } = await kakaoUtil.getToken(code);
+      const { access_token, refresh_token } = await kakaoUtil.getToken(code);
       const userData = await kakaoUtil.getUserData(access_token);
 
       req.session.user = userData;
+      res.cookie("access_token", access_token, {
+        maxAge: 1000 * 60 * 60,
+        httpOnly: true,
+      });
+
+      redisClient.set(userData.userId.toString(), refresh_token);
       res.status(200).json(userData);
     } catch (error) {
       console.error(error);
@@ -43,7 +50,7 @@ const userController = {
   /**
    * @description 카카오 로그아웃
    */
-  getKakaoLogout: async (req, res) => {
+  kakaoLogout: async (req, res) => {
     console.log("===== /logout start =====");
 
     const logoutRedirectUri = kakaoUtil.getLogoutRedirectUri();
@@ -52,6 +59,43 @@ const userController = {
     console.log(`logoutRedirectUri : ${logoutRedirectUri}`);
 
     console.log("===== /logout end =====");
+  },
+  /**
+   * @description 카카오 토큰 갱신하기
+   */
+  updateKakaoAuthToken: async (req, res) => {
+    console.log("===== /kakao/oauth/token start =====");
+
+    redisClient.get("3022227764").then(async (refreshToken) => {
+      const { access_token, refresh_token } = await kakaoUtil.updateToken(
+        refreshToken
+      );
+
+      res.status(200).json({ access_token, refresh_token });
+    });
+
+    console.log("===== /kakao/oauth/token end =====");
+  },
+  /**
+   * @description 카카오 사용자 정보 가져오기
+   */
+  getKakaoUserInfo: async (req, res) => {
+    console.log("===== /kakao/user/me start =====");
+
+    try {
+      console.log("맛있는 쿠키: " + req.cookies["access_token"]);
+      const userData = await kakaoUtil.getUserData(req.cookies["access_token"]);
+      res.status(200).json(userData);
+    } catch (error) {
+      console.error(error);
+
+      const errorData = {
+        message: "Internal server error.. :(",
+      };
+      res.status(500).json(errorData);
+    }
+
+    console.log("===== /kakao/user/me end =====");
   },
   /**
    * @description 사용자 등록
@@ -71,6 +115,24 @@ const userController = {
       .catch((error) => {
         response.json({ success: false, error });
       });
+  },
+  /**
+   * @description 쿠키 가져오기
+   */
+  getCookies: (req, res) => {
+    res.send(req.cookies);
+  },
+  /**
+   * @description 세션 가져오기
+   */
+  getSession: (req, res) => {
+    if (req.session) {
+      console.log("Session : " + JSON.stringify(req.session.user));
+      res.send(req.session);
+    } else {
+      console.log("Sorry, no session...");
+      res.send({ result: "failed!!" });
+    }
   },
 };
 
