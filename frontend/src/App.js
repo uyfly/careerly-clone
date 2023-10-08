28ee1c0de5c1bcc166679f14de34657337f25f71
wooyoung.kim <uyfly@naver.com>
2023-10-08 23:37:29 +0900

    reload 전 세션체크 로직 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,46 +10,63 @@ import Home from "./layouts/Home";
 import Main from "./layouts/Main";
 import Login from "./layouts/Login";
 import Profiles from "./layouts/Profiles";
+import Onboarding from "./layouts/Onboarding";
 import KakaoCallback from "./auth/KakaoCallback";
-import { Provider } from "react-redux";
-import { persistor, store } from "./store/store";
-import { PersistGate } from "redux-persist/integration/react";
-import { useSelector } from "react-redux";
-
-const PrivateRoute = ({ element }) => {
-  const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
-
-  if (element.type.name === "Login") {
-    return isAuthenticated ? <Navigate to="/home" /> : <Login />;
-  } else {
-    return isAuthenticated ? element : <Navigate to="/" />;
-  }
-};
+import { store } from "./store/store";
+import { useDispatch, Provider } from "react-redux";
+import Trends from "./layouts/Trends";
+import Qnas from "./layouts/Qnas";
+import Lounge from "./layouts/Lounge";
+import Job from "./layouts/Job";
+import { useEffect } from "react";
+import { login } from "./store/userSlice";
 
 function App() {
+  // const dispatch = useDispatch();
+
+  useEffect(() => {
+    const handleBeforeUnload = async (e) => {
+      // 새로고침 시에 사용자에게 경고 메시지를 표시할 수 있습니다.
+      e.preventDefault();
+      const result = await fetch("/users/kakao/user/me")
+        .then((res) => res.json())
+        .then((user) => console.log(user));
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      // 컴포넌트가 언마운트될 때 이벤트 리스너를 제거합니다.
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
+
   const router = createBrowserRouter([
     {
       path: "/",
       element: <FullLayout />,
       children: [
         { path: "/", element: <Main /> },
-        { path: "/home", element: <PrivateRoute element={<Home />} /> },
-        { path: "/profiles", element: <PrivateRoute element={<Profiles />} /> },
+        { path: "/home", element: <Home /> },
+        { path: "/trends", element: <Trends /> },
+        { path: "/qnas", element: <Qnas /> },
+        { path: "/job", element: <Job /> },
+        { path: "/lounge", element: <Lounge /> },
+        { path: "/profiles", element: <Profiles /> },
       ],
     },
     {
       path: "/posts/create",
-      element: <PrivateRoute element={<PostCreate />} />,
+      element: <PostCreate />,
     },
-    { path: "/login", element: <PrivateRoute element={<Login />} /> },
+    { path: "/login", element: <Login /> },
     { path: "/auth/kakao/callback", element: <KakaoCallback /> },
+    { path: "/onboarding", element: <Onboarding /> },
   ]);
 
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <RouterProvider router={router} />
-      </PersistGate>
+      <RouterProvider router={router} />
     </Provider>
   );
 }
