7b8535e72e7423aa7604c3c87f0c8ecc84caf690
wooyoung.kim <uyfly@naver.com>
2023-10-08 23:32:27 +0900

    cookieParser 추가

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const session = require("express-session");
+const cookieParser = require("cookie-parser");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
@@ -12,16 +13,25 @@ router.use(
       secure: false, // HTTPS를 사용하는 경우 true로 설정
       maxAge: 3600000, // 세션 만료 시간 (1시간)
     },
-  })
+  }),
+  cookieParser()
 );
 
-// 인가 코드 받기
+// 카카오 인가 코드 받기
 router.get("/kakao/oauth/authorize", userController.getKakaoAuthCode);
 // 카카오 로그인
-router.post("/kakao/login", userController.getKakaoLogin);
+router.post("/kakao/login", userController.kakaoLogin);
 // 카카오 로그아웃
-router.get("/kakao/logout", userController.getKakaoLogout);
+router.get("/kakao/logout", userController.kakaoLogout);
+// 카카오 토큰 갱신하기
+router.post("/kakao/oauth/token", userController.updateKakaoAuthToken);
+// 카카오 사용자 정보 가져오기
+router.get("/kakao/user/me", userController.getKakaoUserInfo);
 // 사용자 등록
 router.post("/register", userController.registerUser);
+// 쿠키 가져오기
+router.get("/getCookies", userController.getCookies);
+// 세션 가져오기
+router.get("/getSession", userController.getSession);
 
 module.exports = router;
