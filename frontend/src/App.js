694bcb7d617f03d81dc984e40eeaff97ea2ac3e2
uyfly <uyfly@naver.com>
2023-10-10 08:38:01 +0900

    새로고침 시 fetch api 삭제

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,34 +13,13 @@ import Profiles from "./layouts/Profiles";
 import Onboarding from "./layouts/Onboarding";
 import KakaoCallback from "./auth/KakaoCallback";
 import { store } from "./store/store";
-import { useDispatch, Provider } from "react-redux";
+import { Provider } from "react-redux";
 import Trends from "./layouts/Trends";
 import Qnas from "./layouts/Qnas";
 import Lounge from "./layouts/Lounge";
 import Job from "./layouts/Job";
-import { useEffect } from "react";
-import { login } from "./store/userSlice";
 
 function App() {
-  // const dispatch = useDispatch();
-
-  useEffect(() => {
-    const handleBeforeUnload = async (e) => {
-      // 새로고침 시에 사용자에게 경고 메시지를 표시할 수 있습니다.
-      e.preventDefault();
-      const result = await fetch("/users/kakao/user/me")
-        .then((res) => res.json())
-        .then((user) => console.log(user));
-    };
-
-    window.addEventListener("beforeunload", handleBeforeUnload);
-
-    return () => {
-      // 컴포넌트가 언마운트될 때 이벤트 리스너를 제거합니다.
-      window.removeEventListener("beforeunload", handleBeforeUnload);
-    };
-  }, []);
-
   const router = createBrowserRouter([
     {
       path: "/",
