7eb56ba4974e555a7ef25cc67ea77d328bfc9040
wooyoung.kim <uyfly@naver.com>
2023-10-14 08:45:24 +0900

    express session을 root 레벨로 변경

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,21 +1,6 @@
 const express = require("express");
-const session = require("express-session");
-const cookieParser = require("cookie-parser");
 const router = express.Router();
 const userController = require("../controllers/userController");
-
-router.use(
-  session({
-    secret: "secret_key", // 변경 필요
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-      secure: false, // HTTPS를 사용하는 경우 true로 설정
-      maxAge: 3600000, // 세션 만료 시간 (1시간)
-    },
-  }),
-  cookieParser()
-);
 
 // 카카오 인가 코드 받기
 router.get("/kakao/oauth/authorize", userController.getKakaoAuthCode);
